39baa17d90ef418c3d7400c4bc084c68cad143b3
JuanArcos <sebas.da200@gmail.com>
2025-05-30 15:13:11 -0500

    vistas footer

---
+++ resources/views/footer/FAQ.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+
+
+<section class="faq-section">
+    <h2 class="faq-title">Preguntas Frecuentes</h2>
+<body>
+
+    <div class="faq-container">
+        <div class="faq-card">
+            <div class="faq-question">¿Cuál es el tiempo de entrega?</div>
+            <div class="faq-answer">Los envíos tardan entre 3 a 5 días hábiles según tu ubicación.</div>
+        </div>
+
+        <div class="faq-card">
+            <div class="faq-question">¿Puedo cambiar la talla?</div>
+            <div class="faq-answer">Sí, puedes realizar cambios dentro de los primeros 7 días.</div>
+        </div>
+
+        <div class="faq-card">
+            <div class="faq-question">¿Qué métodos de pago aceptan?</div>
+            <div class="faq-answer">Aceptamos tarjetas, PSE, y pagos en efectivo a través de Efecty.</div>
+        </div>
+
+        <div class="faq-card">
+            <div class="faq-question">¿Hacen envios a todo el pais?</div>
+            <div class="faq-answer">Sí, hacemos envios nacionales en toda colombia.</div>
+        </div>
+
+        <div class="faq-card">
+            <div class="faq-question">¿Qué hago si el producto llega defectuoso?</div>
+            <div class="faq-answer">Contáctanos dentro de las 48 horas despues de recibir tu producto para hacer un cambio o reembolso.</div>
+        </div>
+    </div>
+</section>
---
+++ resources/views/footer/metodospago.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+<link rel="stylesheet" href="/css/metodo_de_pago.css">
+
+<section class="metodo-pago-section">
+ <h2 class="faq-title">Metodos de pago</h2>
+    <div class="pago-container">
+ <div class="pago-card">
+            <div class="pago-img-box">
+                <img src="/images/metodos_de_pago/mercado_pago.png" alt="Mercadopago">
+            </div>
+            <h3 class="pago-titulo">MercadoPago</h3>
+            <p class="pago-descripcion">El principal metodo de pago de tropical trendz, con el accederas al resto de metodos de pago.</p>
+        </div>
+
+        <div class="pago-card">
+            <div class="pago-img-box">
+                <img src="/images/metodos_de_pago/tarjeta.png" alt="Tarjetas">
+            </div>
+            <h3 class="pago-titulo">Tarjetas de Crédito/Débito</h3>
+            <p class="pago-descripcion">Mercadopago acepta todas las tarjetas Visa y MasterCard.</p>
+        </div>
+
+        <div class="pago-card">
+            <div class="pago-img-box">
+                <img src="/images/metodos_de_pago/pse.png" alt="PSE">
+            </div>
+            <h3 class="pago-titulo">PSE</h3>
+            <p class="pago-descripcion">Pagos seguros directamente desde tu cuenta bancaria.</p>
+        </div>
+
+        <div class="pago-card">
+            <div class="pago-img-box">
+                <img src="/images/metodos_de_pago/efecty.png" alt="Efecty">
+            </div>
+            <h3 class="pago-titulo">Efecty</h3>
+            <p class="pago-descripcion">Paga en efectivo en puntos autorizados Efecty.</p>
+        </div>
+        <div class="pago-card">
+            <div class="pago-img-box">
+                <img src="/images/metodos_de_pago/mercado_puntos.png" alt="Mercado puntos">
+            </div>
+            <h3 class="pago-titulo">Mercado puntos</h3>
+            <p class="pago-descripcion">Puntos acumulados por los clientes de anteriores compras por mercadolibre.</p>
+        </div>
+    </div>
+</section>
